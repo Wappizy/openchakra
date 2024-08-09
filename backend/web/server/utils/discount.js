0372a3bbf716f2fedd66bded4a39ce13acf7438f
Bastien Seree <bastien.seree@wappizy.com>
2024-08-09 17:35:09 +0200

    Refactoring: Fix typo found in discount.js

--- backend/web/server/utils/discount.js
+++ backend/web/server/utils/discount.js
@@ -1,7 +1,6 @@
-
 const computeDiscounts = (/* product_id, quantity*/) => {
   return new Promise((resolve /* , reject*/) => {
     resolve(0)
   })
 }
-module.exports = {computeDiscount}
+module.exports = {computeDiscounts}
